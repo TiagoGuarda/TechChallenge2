0a3f54a58f5b8017f47840e4a64f8ebc6cda0683
Tiago Guarda <tiago.guarda@gmail.com>
2023-09-04 05:53:54 -0300

    Add project files.

---
+++ Blog.API.Data/Models/Noticia.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog.API.Data.Models
+{
+    public class Noticia
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime DataPublicacao { get; set; }
+        public string Autor { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+    }
+}
